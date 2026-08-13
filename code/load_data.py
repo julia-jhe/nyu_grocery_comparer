26563df4c997155963ea291d1e7ac0670bd2b606
Julia <hejiahengarina@gmail.com>
2026-08-13 15:01:52 -0400

    convert numerical data into floats and tested for any conversion errors

--- code/load_data.py
+++ code/load_data.py
@@ -2,6 +2,13 @@ import csv
 
 with open('data/raw/grocery_prices.csv', newline="") as file:
     reader = csv.DictReader(file)
-    
+
     for row in reader:
-        print(row['product_type'], row['store_name'], row['regular_price'])
+        
+        row["package_size"] = float(row["package_size"])
+        row["regular_price"] = float(row["regular_price"])
+
+        try:
+            row['regular_price'] = float(row['regular_price'])
+        except:
+            print(f'Invalid price: {row}')
