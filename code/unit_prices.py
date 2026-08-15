5e31b47eca45747963d0fea10f132f0f00ae1c44
Julia <hejiahengarina@gmail.com>
2026-08-15 18:01:16 -0400

    added unit prices as a column and created a separate dataset

--- code/unit_prices.py
+++ code/unit_prices.py
@@ -1,11 +1,23 @@
 import csv
 
+rows = []
+
 with open ('data/processed/grocery_prices_clean.csv', newline="") as file:
     reader = csv.DictReader(file)
+
+    fieldnames = reader.fieldnames + ['price_per_oz']
 
     for row in reader:
         row["package_size"] = float(row["package_size"])
         row["regular_price"] = float(row["regular_price"])
 
-        price_per_oz = float(round(row["regular_price"] / row["package_size"], 2))
-        print(price_per_oz)
+        row["price_per_oz"] = float(round(row["regular_price"] / row["package_size"], 2))
+
+        rows.append(row)
+
+with open ('data/processed/grocery_prices_unit_prices.csv', 'w', newline="") as new_file:
+
+        writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(rows)
