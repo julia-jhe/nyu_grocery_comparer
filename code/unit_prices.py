f3f06cfd78ae208a18f54ebbd7dfa09e37651ba5
Julia <hejiahengarina@gmail.com>
2026-08-15 17:31:21 -0400

    perform initial price per oz calculations inside loop

---
+++ code/unit_prices.py
@@ -0,0 +1,11 @@
+import csv
+
+with open ('data/processed/grocery_prices_clean.csv', newline="") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        row["package_size"] = float(row["package_size"])
+        row["regular_price"] = float(row["regular_price"])
+        
+        price_per_oz = row["regular_price"] / row["package_size"]
+        print(price_per_oz)
